50f94098cbd45efbf85551fd5ce6f014174f3c86
Alex Delgado <gadc1996@gmail.com>
2021-01-18 16:38:59 -0700

    animated background

---
+++ assets/js/vanta.config.js
@@ -0,0 +1,14 @@
+VANTA.NET({
+    el: "#parallax",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x032c6b,
+    backgroundColor: 0x040d21,
+    maxDistance: 22.00,
+    spacing: 14.00
+  })
